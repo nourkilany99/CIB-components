8d63e90bf197c984fc0e638ca7d17838f5b822c5
nour ashraf elkilany <nourelkilany@Nours-MacBook-Pro-2.local>
2025-10-30 09:00:19 +0300

    Header&Nav

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,23 @@
-import React, {} from 'react';
+import React from 'react';
 import Logo from '../img/CibLogo.png';
-import "./Header.css"
-
+import './Header.css';
+import Nav from './Nav';
 
 const Header = () => {
-    return ( 
+  return (
+    <header>
+      <div className="div_logo">
+        <img className="logo" src={Logo} alt="logo" />
+      </div>
+      <Nav title="About us" />
+      <Nav title="Investor relation" />
+      <Nav title="Responsible Banking" />
+      <Nav title="Newsroom" />
+      <Nav title="Learning center" />
+      <Nav title="careers" />
+      <Nav title="Other" />
+    </header>
+  );
+};
 
-        <header>
-            <div className='div_logo'>
-               <img className='logo' src={Logo} alt="logo" />
-            </div>
-        </header>
-
-     );
-}
- 
 export default Header;
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,13 @@
+import React, {} from 'react';
+import "./Nav.css"
+
+
+const Nav = ({ title }) => {
+  return (
+    <>
+      <p className="title_nav1">{title}</p>
+     </> 
+  );
+};
+ 
+export default Nav;
