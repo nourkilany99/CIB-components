97296c025ef648e018672ebfc8808641ea54131c
nour ashraf elkilany <nourelkilany@Nours-MacBook-Pro-2.local>
2025-10-31 10:58:13 +0200

    Small_menu

---
+++ src/common/Smallmenu.jsx
@@ -0,0 +1,14 @@
+import React, { } from 'react';
+import './Smallmenu.css';
+
+
+const Smallmenu = (props) => {
+    return ( 
+        <>
+         <p className='p_d_small_menu'>{props.title}</p>
+        
+        </>
+     );
+}
+ 
+export default Smallmenu;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,23 @@ import React from 'react';
 import Header from '../Components/Header';
 import './Home.css';
 import Hero from '../Components/Hero';
+import Smallmenu from '../common/Smallmenu';
+import '../common/Smallmenu.css';
 
 const Home = () => {
   return (
     <>
       <Header />
       <Hero />
+
+      <div className='small_menu_d'>
+      <Smallmenu title="Apply Online"/>
+      <Smallmenu title="News"/>
+      <Smallmenu title="Blog Articles"/>
+      <Smallmenu title="CIB International"/>
+      <Smallmenu title="Sustainable Finance"/>
+      <Smallmenu title="Awards"/>
+      </div>
     </>
   );
 };
